b425027ce52d46397fc2fafb67e1eb07408f1d88
James Wenzel <jameswenzel@users.noreply.github.com>
2023-01-10 12:44:01 -0800

    initial commit

--- test/BaseTest.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-import { Test } from "forge-std/Test.sol";
-
-contract BaseTest is Test {
-    function setUp() public virtual { }
-}
--- test/Prose.t.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-import { BaseTest } from "test/BaseTest.sol";
-
-contract ProseTest is BaseTest { }
